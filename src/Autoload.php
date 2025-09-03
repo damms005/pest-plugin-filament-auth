5207293d8842072a8d8f6a032da72e4501809dfe
Damilola Olowookere <damms005@gmail.com>
2025-09-03 12:02:56 +0100

    Setup plugin structure

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,17 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Pest\PluginName;
+namespace Pest\FilamentAuth;
 
-use Pest\Plugin;
-use PHPUnit\Framework\TestCase;
-
-Plugin::uses(Example::class);
-
-/**
- * @return TestCase
- */
-function example(string $argument)
-{
-    return test()->example(...func_get_args()); // @phpstan-ignore-line
-}
+pest()->preset('testAuth', function () {
+    return [
+        expect(true)->toBeTrue(),
+    ];
+});
